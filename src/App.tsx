483eac1017f2a2235aa7da240ec5a01caad7bf50
Giulio Denardi <giulio.lpdenardi@gmail.com>
2021-04-20 10:55:20 -0300

    feat: add page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box display="relative" className="PeopleBox">
+      </Box>
+
+      <Box className="ChatBox" display="flex" justifyContent="space-around" alignItems="center">
+        <TextField className="ChatInput" label="Message" fullWidth={true} />
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+      </Box>
+
     </div>
   );
 }
